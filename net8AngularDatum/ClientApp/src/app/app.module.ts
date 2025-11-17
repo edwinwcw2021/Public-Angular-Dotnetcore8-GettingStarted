5ca521bef09aeecb8ccc994db41b39289a829b83
Edwin Wong <wongewc@gmail.com>
2025-11-17 17:20:21 +0000

    angular 18

--- net8AngularDatum/ClientApp/src/app/app.module.ts
+++ net8AngularDatum/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -14,29 +14,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
 import { AboutComponent } from './about/about.component';
 
-@NgModule({
-  declarations: [
-    MinuteValidatorDirective,
-    DegreeValidatorDirective,
-    HkeValidatorDirective,
-    HknValidatorDirective,
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    AboutComponent
-  ],
-  imports: [
-    MaterialModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'about', component: AboutComponent  }
-    ]),
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        MinuteValidatorDirective,
+        DegreeValidatorDirective,
+        HkeValidatorDirective,
+        HknValidatorDirective,
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        AboutComponent
+    ],
+    bootstrap: [AppComponent], imports: [MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent, pathMatch: 'full' },
+            { path: 'about', component: AboutComponent }
+        ]),
+        BrowserAnimationsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
