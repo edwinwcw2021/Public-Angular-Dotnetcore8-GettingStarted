7a88295710c2343474e599f36632cbf8a575f5aa
Edwin Wong <wongewc@gmail.com>
2025-11-22 21:08:41 +0000

    security update

--- net8AngularDatum/ClientApp/src/app/app.component.ts
+++ net8AngularDatum/ClientApp/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    const root = document.querySelector('app-root');
+    root?.removeAttribute('ng-version');    
+  }    
 }
